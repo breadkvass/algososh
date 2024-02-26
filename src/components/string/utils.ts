7d8253dd99d0db540f42e9b524e69df39163e71d
breadkvass <breadkvass@gmail.com>
2024-02-26 15:52:15 +0300

    upd StringComponent

---
+++ src/components/string/utils.ts
@@ -0,0 +1,28 @@
+type Reverse = { arr: string[]; swapping: number[]; done: number[] };
+
+export const delay = (time: number) => new Promise<void>((resolve) => setTimeout(resolve, time));
+
+const swap = (arr: string[], index1: number, index2: number) => {
+    let temp: string = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+}
+  
+export const getReverse = (str: string): Reverse[] => {
+    const arr: string[] = str.split('');
+    const reversed: Reverse[] = [{ arr: arr.slice(), swapping: [], done: [] }];
+
+    for (let i = 0; i < arr.length / 2; i++) {
+        const index2: number = str.length - i - 1;
+        const length: number = reversed.length;
+
+        reversed.push({ ...reversed[length - 1], swapping: [i, index2] });
+        swap(arr, i, index2);
+        reversed.push({
+            ...reversed[length - 1],
+            arr: arr.slice(),
+            done: reversed[length - 1].done.concat([i, index2]),
+        });
+    }
+    return reversed;
+};
