d29af4ccbca25ac65f5da1266d547317a96ebdee
breadkvass <breadkvass@gmail.com>
2024-02-27 18:15:38 +0300

    add getRandomArr to SortingPage

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { SyntheticEvent, useState } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import styles from './sorting-page.module.css';
 import { Column } from "../ui/column/column";
 import { RadioInput } from "../ui/radio-input/radio-input";
 import { Button } from "../ui/button/button";
 import { Direction } from "../../types/direction";
+import { getRandomArr } from "./utils";
 
 export const SortingPage: React.FC = () => {
+  const [ dataRandomArr, setDataRandomArr ] = useState<{arr: number[]; loading: boolean}>({
+    arr: [],
+    loading: false
+  });
 
-  let arr: number[] = [25, 70, 10, 45, 80];
+  const handleClick = (e: SyntheticEvent) => {
+    e.preventDefault();
+    setDataRandomArr((prev) => ({ ...prev, loading: true }));
+    setDataRandomArr((prev) => ({ ...prev, arr: getRandomArr() }));
+    dataRandomArr.arr && setDataRandomArr((prev) => ({ ...prev, loading: false }));
+  }
+
   return (
     <SolutionLayout title="Сортировка массива">
       <div className={styles.filtres}>
@@ -20,10 +31,10 @@ export const SortingPage: React.FC = () => {
           <Button sorting={Direction.Ascending} text='По возрастанию' />
           <Button sorting={Direction.Descending} text='По убыванию' />
         </div>
-        <Button text='Новый массив' />
+        <Button text='Новый массив' onClick={handleClick}/>
       </div>
       <div className={styles.columns}>
-        {arr.map((item, i) => <Column index={item}/>)}
+        {dataRandomArr.arr.map((item, i) => <Column index={item} key={i}/>)}
       </div>
     </SolutionLayout>
   );
