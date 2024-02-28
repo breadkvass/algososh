6cb4dbde8cb2c8653f7afd4ad016ef183b1f37cb
breadkvass <breadkvass@gmail.com>
2024-02-28 15:12:07 +0300

    add StackPage layout

--- src/components/stack-page/stack-page.tsx
+++ src/components/stack-page/stack-page.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import styles from './stack-page.module.css'
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
 
 export const StackPage: React.FC = () => {
+
   return (
     <SolutionLayout title="Стек">
-
+      <form className={styles.form}>
+        <div className={styles.edit}>
+          <Input isLimitText maxLength={4} />
+          <Button text='Добавить' />
+          <Button text='Удалить' />
+        </div>
+        <Button text='Очистить' />
+      </form>
     </SolutionLayout>
   );
 };
