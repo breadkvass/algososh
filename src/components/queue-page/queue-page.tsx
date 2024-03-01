89d8e184c46029b3f4a426a91e14c07bf098c241
breadkvass <breadkvass@gmail.com>
2024-03-01 21:00:06 +0300

    add QueuePage layout

--- src/components/queue-page/queue-page.tsx
+++ src/components/queue-page/queue-page.tsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
+import styles from './queue-page.module.css';
 
 export const QueuePage: React.FC = () => {
+  const [inputValue, setInputValue] = useState('');
+  let array: string[] = ['', '', '', '', '', '', ''];
   return (
-    <SolutionLayout title="Очередь">
-
+    <SolutionLayout title="Стек">
+      <form className={styles.form} onSubmit={() => {}}>
+        <div className={styles.edit}>
+          <Input isLimitText maxLength={4} value={''} onChange={(e) => setInputValue(e.currentTarget.value)} />
+          <Button type="submit" text="Добавить" disabled={!inputValue} isLoader={false} />
+          <Button onClick={() => {}} text="Удалить" disabled={false} isLoader={false} />
+        </div>
+        <Button disabled={false} onClick={() => {}} text="Очистить" />
+      </form>
+      <div className={styles.circles}>
+        {array.map((item, i) => (
+          <Circle letter={item} key={i} index={i} />
+        ))}
+      </div>
     </SolutionLayout>
   );
 };
