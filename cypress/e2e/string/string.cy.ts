f5fb08c2de5eb282040ecb9e5641742d62ea1f14
Anastasiya <sulimova.a.m@yandex.ru>
2024-03-27 15:46:33 +0300

    add string test

---
+++ cypress/e2e/string/string.cy.ts
@@ -0,0 +1,50 @@
+import 'cypress-wait-until';
+
+describe('Проверка строки', function() {
+    const test = 'string';
+    const circles = () => {
+        return cy.get('div[class^="string_circles"]').children()
+    }
+
+    beforeEach(function() {
+        cy.visit('http://localhost:3000/');
+        cy.get('[href="/recursion"]').click();
+    });
+
+    it('если в инпуте пусто, то кнопка добавления недоступна', function() {
+        cy.get('[type="text"]').clear();
+        cy.get('button').get('[type="submit"]').should('be.disabled');
+    });
+
+    it('строка разворачивается корректно', () => {
+        cy.get('[type="text"]').type(test)
+        cy.get('[type="submit"]').click();
+
+        circles().should('have.length', test.length);
+
+        for (let i = 0; i < test.length / 2; i++) {
+            circles()
+                .eq(i)
+                .find('div[class^="circle_circle"]')
+                .should('have.attr', 'class')
+                .and('include', 'changing')
+                .waitUntil(() => circles()
+                    .find('div[class^="circle_circle"]')
+                    .should('have.attr', 'class')
+                    .should('include', 'modified'));
+        }
+    })
+
+    it('анимация разворота строки работает корректно', () => {
+        cy.get('[type="text"]').type(test)
+        cy.get('[type="submit"]').click();
+
+        circles().should('have.length', test.length);
+
+        for (let i = 0; i < test.length / 2; i++) {
+            circles()
+                .eq(i)
+                .should('contain.text', test[test.length - i - 1])
+        }  
+    })
+})
